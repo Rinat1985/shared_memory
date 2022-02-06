99d8307a3c2d063cc65717e3796faa90f237d8eb
Rinat1985 <shigapov.rd@gmail.com>
2022-02-06 15:44:18 +0400

    fix server

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -9,14 +9,19 @@ int main(int argc, char *argv[]){
     using namespace boost::interprocess;
     typedef boost::interprocess::allocator<char, boost::interprocess::managed_windows_shared_memory::segment_manager> CharAllocator;
     typedef boost::interprocess::basic_string<char, std::char_traits<char>, CharAllocator> string;
-    managed_windows_shared_memory shm(create_only, "MySharedMemory", 10240);
+    managed_windows_shared_memory shm(open_or_create, "MySharedMemory", 10240);
+
+
     while(1){
         std::cout << "Input something:" << std::endl;
         std::string str;
         std::cin >> str;
         string * mess = shm.find_or_construct<string>("Message")(str, shm.get_segment_manager());
+
+        std::cout << " find_and_constr = " << *mess << std::endl;
         std::pair<string *, std::size_t> ret = shm.find<string>("Message");
         std::cout << *(ret.first) << std::endl;
+        std::system("pause");
     }
 
     return 0;
